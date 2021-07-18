eb0d868ba6fb5771e35304b65a8ec214facdd4a8
Bilo Lwabona <bilo@revix.com>
2021-07-18 20:55:29 +0200

    added storybook

---
+++ src/styles/index.ts
@@ -0,0 +1,23 @@
+export const boxShadow = `
+    0 1px 1px rgba(0,0,0,0.05),
+    0 8px 8px rgba(0,0,0,0.05),
+    0 12px 12px rgba(0,0,0,0.05)
+`
+
+export const flexRow = {
+    display: 'flex',
+    flexDirection: 'row'
+}
+
+export const flexCol = {
+    display: 'flex',
+    flexDirection: 'column'
+}
+
+export const flexWrap = {
+    flexWrap: 'wrap'
+}
+
+export const flexWrapReverse = {
+    flexWrap: 'wrap-reverse'
+}
