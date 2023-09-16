5e3a7dc460e6884e062fb872a0da268593c2bfad
Bilo Lwabona <bilo@revix.com>
2023-09-16 03:33:22 +0200

    feat: updated gallery models

--- lib/models/galleries.d.ts
+++ lib/models/galleries.d.ts
@@ -2,22 +2,34 @@ export interface IImage {
     uri?: string;
     data?: string;
     name?: string;
+    type: IMediaType;
     id: string;
 }
 export interface ISound {
     uri?: string;
     data?: string;
     name?: string;
+    type: IMediaType;
     id: string;
 }
 export interface IVideo {
     uri?: string;
     data?: string;
     name?: string;
+    type: IMediaType;
+    id: string;
+}
+export type IMediaType = 'Image' | 'Sound' | 'Video';
+export interface IMedia {
+    uri?: string;
+    data?: string;
+    name?: string;
+    type: IMediaType;
     id: string;
 }
 export interface IGallery {
     id: string;
     name: string;
+    type: IMediaType;
     media: IImage[] | ISound[] | IVideo[];
 }
--- src/models/galleries.ts
+++ src/models/galleries.ts
@@ -2,25 +2,39 @@ export interface IImage {
     uri?: string;
     data?: string;
     name?: string;
-    id: string
+    type: IMediaType;
+    id: string;
 }
 
 export interface ISound {
     uri?: string;
     data?: string;
     name?: string;
-    id: string
+    type: IMediaType;
+    id: string;
 }
 
 export interface IVideo {
     uri?: string;
     data?: string;
     name?: string;
-    id: string
+    type: IMediaType;
+    id: string;
+}
+
+export type IMediaType = 'Image' | 'Sound' | 'Video'
+
+export interface IMedia {
+    uri?: string;
+    data?: string;
+    name?: string;
+    type: IMediaType;
+    id: string;
 }
 
 export interface IGallery {
     id: string;
     name: string;
-    media: IImage[] | ISound[] | IVideo[]
+    type: IMediaType;
+    media: IImage[] | ISound[] | IVideo[];
 }
