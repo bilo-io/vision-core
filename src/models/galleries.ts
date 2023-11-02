5afbb974ac1c42cc1ebe78adc84cb781906e716a
Bilo Lwabona <bilo@revix.com>
2023-11-02 05:08:52 +0200

    feat: add documents to galleries

--- src/models/galleries.ts
+++ src/models/galleries.ts
@@ -1,10 +1,18 @@
+import { ICharacter } from "./characters";
+import { IDialogue } from "./dialogues";
+import { IDocument } from "./documents";
+
 import { IImage } from "./images";
 import { ISound } from "./sounds";
 import { IVideo } from "./videos";
-import { ICharacter } from "./characters";
-import { IDialogue } from "./dialogues";
 
-export type IMediaType = 'Image' | 'Sound' | 'Video' | 'Dialogue' | 'Character'
+export type IMediaType =
+    'Character' |
+    'Dialogue' |
+    'Document' |
+    'Image' |
+    'Sound' |
+    'Video';
 
 export interface IMedia {
     uri?: string;
@@ -18,5 +26,11 @@ export interface IGallery {
     id: string;
     name: string;
     type: IMediaType;
-    media: IImage[] | ISound[] | IVideo[] | IDialogue[] | ICharacter[];
+    media:
+        ICharacter[] |
+        IDocument[] |
+        IDialogue[] |
+        IImage[] |
+        ISound[] |
+        IVideo[];
 }
