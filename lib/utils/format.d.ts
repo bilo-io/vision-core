6efc22f881611b822d966820648579d24f159116
Bilo Lwabona <bilo@revix.com>
2023-11-13 22:50:53 +0200

    feat: add formatDuration util

--- lib/utils/format.d.ts
+++ lib/utils/format.d.ts
@@ -1,1 +1,2 @@
 export declare function numberWithCommas(x: number): string;
+export declare const formatDuration: (currentTime: number, totalDuration: number) => string;
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,3 +1,20 @@
 export function numberWithCommas(x: number) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export const formatDuration = (currentTime: number, totalDuration: number) => {
+
+    const format = (value: number): string => {
+        const paddedValue = value < 10 ? `0${value}` : `${value}`;
+        return paddedValue;
+    };
+
+    const formatSeconds = (seconds: number): string => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = Math.floor(seconds % 60);
+        return `${format(minutes)}:${format(remainingSeconds)}`;
+    };
+
+    return `${formatSeconds(currentTime)} / ${formatSeconds(totalDuration)}`;
+}
+
