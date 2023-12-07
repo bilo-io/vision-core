0ff91959be57ca495a72e59d19f11479b681788a
Bilo Lwabona <bilo@revix.com>
2023-12-07 08:14:11 +0200

    feat: updated features models

--- lib/models/features.d.ts
+++ lib/models/features.d.ts
@@ -1,3 +1,4 @@
+export type IFeatureStatus = 'concept' | 'pre-alpha' | 'alpha' | 'beta' | 'ready';
 export interface IFeature {
     /** `key` is the identifier of the feature */
     key: string;
@@ -5,4 +6,8 @@ export interface IFeature {
     isActive: boolean;
     /** `message` is used during maintenance to display information when the feature is down */
     message: string;
+    /** `status` is the state the feature is in (`alpha`, `beta`, etc.) */
+    status: IFeatureStatus;
+    /** `updatedAt` is the last date at which the feature was updated */
+    updatedAt?: string;
 }
