4d92063807fd67ff724b3a2463f782557ed7c394
Bilo Lwabona <bilo@revix.com>
2023-12-07 08:06:51 +0200

    chore: fixed isActive type

--- src/models/features.ts
+++ src/models/features.ts
@@ -1,5 +1,8 @@
 export interface IFeature {
+    /** `key` is the identifier of the feature */
     key: string;
-    isActive: string;
+    /** `isActive` is a boolean to determine whether the feature is available or under maintenance */
+    isActive: boolean;
+    /** `message` is used during maintenance to display information when the feature is down */
     message: string;
 }
