a2eba06d34720e21ed3fc40e8cf26a7cf20d8411
Bilo Lwabona <bilo@revix.com>
2023-12-11 20:45:27 +0200

    feat: add images to project

--- lib/models/projects.d.ts
+++ lib/models/projects.d.ts
@@ -3,8 +3,19 @@ export interface IProject {
     id: string | number;
     /** `name`:  the human-friendly name of the project */
     name: string;
-    /** `a project image`:  the various sections the project has */
+    /** `image`:  the image of a project (to be `deprecated`) */
     image?: string;
+    /** `images`?:  the various different images of a projec, like an app store page  */
+    images?: {
+        /** `coverImage` used to show the general splash/cover/background art */
+        coverImage: string;
+        /**  `tileImage` is like the thumbnail */
+        tileImage: string;
+        /**  `titleImage` is to render the game title in a specific image (custom artwork) */
+        titleImage: string;
+        /** `slides` are general images to show in the slideshow */
+        slides?: string[];
+    };
     /**`a list of features`: the sidebar tabs offered by vision studio */
     features?: string[];
     /** `a project summary`: */
